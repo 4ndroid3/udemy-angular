276c101a10c5af18b0e2c879f6a9f6b44ebcdfbf
4ndroid3 <andijaimo@gmail.com>
2022-05-19 17:41:58 -0300

    termina app de paises

--- paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
+++ paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -5,13 +5,21 @@ import { PaisService } from '../../services/pais.service';
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
-  styleUrls: ['./por-pais.component.css']
+  styles: [
+    `
+    li {
+      cursor: pointer;
+    }
+    `
+  ]
 })
 export class PorPaisComponent implements OnInit {
 
   termino = '';
   hayError: boolean = false;
-  country: Country[] = []
+  country: Country[] = [];
+  paisesSugeridos: Country[] = [];
+  mostrarSugerencias: boolean = false;
 
   constructor( private paisService: PaisService ) { }
 
@@ -19,8 +27,10 @@ export class PorPaisComponent implements OnInit {
   }
 
   buscar( termino: string ) {
+    this.mostrarSugerencias = false;
     this.hayError = false;
     this.termino = termino
+
     this.paisService.buscarPais(termino)
     .subscribe( (paises: Country[]) => {
       this.country = paises;
@@ -32,6 +42,20 @@ export class PorPaisComponent implements OnInit {
 
   sugerencias( termino: string ) {
     this.hayError = false;
+    this.termino = termino
+    this.mostrarSugerencias = true;
+
+    this.paisService.buscarPais( termino )
+      .subscribe(
+        paises => this.paisesSugeridos = paises.splice(0,5),
+        (err) => this.paisesSugeridos = []
+      );
+
+  }
+
+  buscarSugerido( termino: string ) {
+    this.buscar( termino );
+
   }
 
 }
