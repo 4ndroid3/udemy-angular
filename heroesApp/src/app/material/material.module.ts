3467a730170c2e8b7d647d5e5c1b5e1c59ec5afd
4ndroid3 <andijaimo@gmail.com>
2022-07-28 23:18:18 -0300

    terminando primera parte de heroes app

--- heroesApp/src/app/material/material.module.ts
+++ heroesApp/src/app/material/material.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -11,6 +13,8 @@ import { MatCardModule } from '@angular/material/card';
   exports: [
     MatButtonModule,
     MatCardModule,
+    MatGridListModule,
+    MatProgressSpinnerModule,
     MatIconModule,
     MatListModule,
     MatSidenavModule,
