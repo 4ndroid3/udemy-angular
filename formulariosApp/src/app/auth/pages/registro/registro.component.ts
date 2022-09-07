cebf415035d3e965a33d102afd715fddcbae8a23
4ndroid3 <andijaimo@gmail.com>
2022-09-07 09:30:11 -0300

    Termina con formularios reactivos, validaciones

--- formulariosApp/src/app/auth/pages/registro/registro.component.ts
+++ formulariosApp/src/app/auth/pages/registro/registro.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EmailValidatorService } from 'src/app/shared/validator/email-validator.service';
 import { ValidatorService } from 'src/app/shared/validator/validator.service';
 
 @Component({
@@ -11,7 +12,7 @@ export class RegistroComponent implements OnInit {
 
   miFormulario: FormGroup = this.fb.group({
     nombre: ['', [Validators.required, Validators.pattern( this.vs.nombreApellidoPattern ) ] ],
-    email: ['', [Validators.required, Validators.pattern( this.vs.emailPattern )] ],
+    email: ['', [Validators.required, Validators.pattern( this.vs.emailPattern )], [ this.eValidator ] ],
     username: ['', [Validators.required, this.vs.noPuedeSerStrider] ],
     password: ['', [Validators.required, Validators.minLength(6)] ],
     password2: ['', [Validators.required], ],
@@ -21,8 +22,25 @@ export class RegistroComponent implements OnInit {
 
   constructor(
     private fb: FormBuilder,
-    private vs: ValidatorService
+    private vs: ValidatorService,
+    private eValidator: EmailValidatorService,
   ) { }
+
+  get emailErrorMsg() {
+
+    const errors = this.miFormulario.get('email')?.errors;
+    if (errors?.required) {
+      return 'Email es obligatorio';
+    }
+    else if (errors?.pattern) {
+      return 'El valor ingresado no tiene formato de email';
+    }
+    else if (errors?.emailTomado) {
+      return 'Email ya tomado';
+    }
+    return '';
+    
+  }
 
   ngOnInit(): void {
   }
