e31b1e8718e3b507dc9ef76df005c82b71432716
4ndroid3 <andijaimo@gmail.com>
2022-09-08 10:23:02 -0300

    comienza selectores anidados, logica http

--- selectores/src/app/paises/pages/selector-page/selector-page.component.ts
+++ selectores/src/app/paises/pages/selector-page/selector-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PaisSmall } from '../../interfaces/paises.interface';
+import { PaisesService } from '../../services/paises.service';
 
 @Component({
   selector: 'app-selector-page',
@@ -10,11 +12,29 @@ export class SelectorPageComponent implements OnInit {
 
   miFormulario: FormGroup = this.fb.group({
     region: ['', Validators.required],
+    pais: ['', Validators.required],
   })
 
-  constructor( private fb: FormBuilder ) { }
+  // Llenar Selectores
+  regiones: string[] = [];
+  paises: PaisSmall[] = [];
+
+  constructor( private fb: FormBuilder,
+               private paisesService: PaisesService ) { }
 
   ngOnInit(): void {
+    this.regiones = this.paisesService.regiones;
+
+    // Cuando cambia la region
+    this.miFormulario.get('region')?.valueChanges
+      .subscribe( region => {
+        console.log(region)
+        this.paisesService.getPaisesPorRegion( region )
+          .subscribe( paises => {
+            console.log(paises);
+            this.paises = paises;
+          })
+      })
   }
 
   guardar() {
--- selectores/src/app/paises/services/paises.service.ts
+++ selectores/src/app/paises/services/paises.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PaisSmall } from '../interfaces/paises.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaisesService {
 
-  constructor() { }
+  private _baseUrl: string = 'https://restcountries.com/v2';
+  private _regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+
+  get regiones(): string[] {
+    return [...this._regiones];
+  }
+  // https://restcountries.com/v2/region/americas?fields=alpha3Code;name
+  constructor(private http: HttpClient) { }
+
+  getPaisesPorRegion( region: string ): Observable<PaisSmall[]> {
+    const url = `${ this._baseUrl }/region/${ region }?fields=alpha3Code,name`;
+    return this.http.get<PaisSmall[]>( url );
+  }
+
 }
