f09cf795273035a4a54444541c2170cde358557e
4ndroid3 <andijaimo@gmail.com>
2022-10-28 19:33:30 -0300

    Configuraciones y desestructuracion de codigo

--- auth-server-node/index.js
+++ auth-server-node/index.js
@@ -1,16 +1,22 @@
 const express = require('express');
+const cors = require('cors');
+require('dotenv').config(); // Al levantarse este import disponibiliza las variables de entorno en el archivo .env
 
 
 
 // crear el servidor de express.
 const app = express();
 
+// CORS
+app.use( cors() );
 
+// Lectura y parseo del body
+app.use( express.json() );
 
 // Rutas
 app.use( '/api/auth', require('./routes/auth') );
 
 
-app.listen( 4000, () => {
-    console.log(`Servidor corriendo en puerto ${ 4000 }`);
+app.listen( process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
 });
