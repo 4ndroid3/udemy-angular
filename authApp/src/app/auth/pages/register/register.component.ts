14e4921961ba297cb099fe967e9471d4086bb2c5
4ndroid3 <andijaimo@gmail.com>
2022-11-11 08:52:39 -0300

    Login Completo

---
+++ authApp/src/app/auth/pages/register/register.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  miFormulario: FormGroup = this.fb.group({
+    name: ['', [ Validators.required, Validators.minLength(3) ]],
+    email: ['', [ Validators.required, Validators.email ]],
+    password: ['', [ Validators.required, Validators.minLength(6) ]]
+});
+
+  constructor( private fb: FormBuilder ) { }
+
+  ngOnInit(): void {
+  }
+
+
+  registro() {
+    console.log(this.miFormulario.value);
+    console.log(this.miFormulario.valid);
+  }
+}
