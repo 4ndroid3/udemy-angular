6d4915e0f4dab96bb062bbe32e818d9a00a587a1
4ndroid3 <andijaimo@gmail.com>
2022-11-17 12:46:33 -0300

    Termina el login, persiste usuario logeado

---
+++ authApp/src/app/auth/services/auth.service.ts
@@ -0,0 +1,66 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { AuthResponse, Usuario } from '../interfaces/interfaces';
+import { catchError, map, tap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private _baseUrl: string = environment.baseUrl;
+  private _usuario!: Usuario;
+
+  get usuario() {
+    return { ...this._usuario };
+  }
+
+  constructor( private http: HttpClient ) { }
+
+  login( email: string, password: string ) {
+
+    const url = `${ this._baseUrl }/auth`;
+    const body = { email, password };
+
+    return this.http.post<AuthResponse>( url, body )
+      .pipe(
+        tap( resp => {
+          if ( resp.ok ) {
+            localStorage.setItem('token', resp.token!);
+            this._usuario = {
+              uid: resp.uid!,
+              name: resp.name!
+            }
+          }
+        }),
+        map( resp => resp.ok ),
+        catchError( err => of(err.error.msg) ),
+      );
+  }
+
+  validarToken(): Observable<boolean> {
+
+    const url = `${ this._baseUrl }/auth/renew`;
+    const headers = new HttpHeaders()
+      .set('x-token', localStorage.getItem('token') || '');
+
+    return this.http.get<AuthResponse>( url, { headers } )
+      .pipe(
+        map( resp => {
+          localStorage.setItem('token', resp.token!);
+          this._usuario = {
+            uid: resp.uid!,
+            name: resp.name!
+          }
+          return resp.ok;
+        }),
+        catchError( err => of(false) )
+      );
+  }
+
+  logout() {
+    localStorage.clear();
+  }
+}
--- authApp/src/app/protected/dashboard/dashboard.component.ts
+++ authApp/src/app/protected/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,13 +9,20 @@ import { Router } from '@angular/router';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor( private router: Router ) { }
+  get usuario() {
+    return this.authService.usuario;
+  }
+
+  constructor( private router: Router,
+               private authService: AuthService ) { }
 
   ngOnInit(): void {
+    
   }
 
   logout() {
     this.router.navigateByUrl('/auth');
+    this.authService.logout();
   }
 
 }
